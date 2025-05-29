fccfc521ce0acf7c23c5abaaba8a734c54adb161
Jyoti Kumari <210jg021@gmail.com>
2025-05-29 17:10:44 +0530

    Initial project commit

---
+++ JyotiKumari_22ucs096/src/test/java/com/example/JyotiKumari_22ucs096/JyotiKumari22ucs096ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JyotiKumari_22ucs096;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JyotiKumari22ucs096ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
